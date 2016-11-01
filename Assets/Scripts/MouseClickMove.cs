f18e3f46f086508e12e645ee6d46039d3222f342
Jarek Chaninowicz <jarek.chaninowicz@razorbear.com>
2016-11-01 18:51:57 +0100

    added localhost multiplayer

---
+++ Assets/Scripts/MouseClickMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MouseClickMove : NetworkBehaviour {
+	Vector3 newPosition;
+	float duration = 5.0f;
+	float objectOriginalYAxis;
+	// Use this for initialization
+	void Start () {
+		objectOriginalYAxis = 1.0f;
+		newPosition = transform.position;
+		newPosition.y = objectOriginalYAxis;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit)) {
+				newPosition = hit.point;
+				newPosition.y = objectOriginalYAxis;
+			}
+		}
+
+		transform.position = Vector3.Lerp(transform.position, newPosition, 1 / (duration * (Vector3.Distance (transform.position, newPosition))));
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+	
+	public Camera camera;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void OnStartLocalPlayer() {
+		camera.enabled = true;
+		GetComponent<MeshRenderer>().material.color = Color.green;
+	}
+}
